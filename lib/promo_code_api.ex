d4544732a2a87fd3f550e79b339674045ae7d05a
vickris <christophervundi@gmail.com>
2019-07-10 15:33:55 +0300

    Update code for requesting ride

--- lib/promo_code_api.ex
+++ lib/promo_code_api.ex
@@ -6,8 +6,25 @@ defmodule PromoCodeApi do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias PromoCodeApi.Repo
+  alias PromoCodeApi.SafeBoda.Promo
+  alias Distance.GreatCircle
+  alias PromoCodeApi.SafeBoda
 
   def request_boda(origin, destination, code) do
-    Distance.GreatCircle.distance({origin.longitude, origin.latitude}, {destination.longitude, destination.latitude})
+    promo = Promo |> Repo.get_by!(code: code) |> Repo.preload([event: :location])
+
+    promo_radius = promo.radius
+    event_location = promo.event.location
+
+    radius_origin_to_event = (GreatCircle.distance({origin.longitude, origin.latitude}, {event_location.longitude, event_location.latitude}) / 1000)
+    radius_event_to_destination = (GreatCircle.distance({event_location.longitude, event_location.latitude}, {destination.longitude, destination.latitude}) / 1000)
+
+    if radius_origin_to_event <= promo_radius || radius_event_to_destination <= promo_radius do
+        SafeBoda.update_promo(promo, %{is_deactivated: true})
+        IO.puts "Valid promo"
+    else
+        IO.puts "Cant Use Promo"
+    end
   end
 end
