e4f0585cd6d96977f5fafb85b77fb3575ee7235b
vickris <christophervundi@gmail.com>
2019-07-10 16:48:18 +0300

    Some error handling

--- lib/promo_code_api.ex
+++ lib/promo_code_api.ex
@@ -13,18 +13,33 @@ defmodule PromoCodeApi do
 
   def request_boda(origin, destination, code) do
     promo = Promo |> Repo.get_by!(code: code) |> Repo.preload([event: :location])
+    is_valid = check_promo_validity(promo)
 
-    promo_radius = promo.radius
-    event_location = promo.event.location
+    case is_valid do
+      {:ok, "valid"} ->
+        promo_radius = promo.radius
+        event_location = promo.event.location
 
-    radius_origin_to_event = (GreatCircle.distance({origin.longitude, origin.latitude}, {event_location.longitude, event_location.latitude}) / 1000)
-    radius_event_to_destination = (GreatCircle.distance({event_location.longitude, event_location.latitude}, {destination.longitude, destination.latitude}) / 1000)
+        radius_origin_to_event = (GreatCircle.distance({origin.longitude, origin.latitude}, {event_location.longitude, event_location.latitude}) / 1000)
+        radius_event_to_destination = (GreatCircle.distance({event_location.longitude, event_location.latitude}, {destination.longitude, destination.latitude}) / 1000)
 
-    if radius_origin_to_event <= promo_radius || radius_event_to_destination <= promo_radius do
-        SafeBoda.update_promo(promo, %{is_deactivated: true})
-        IO.puts "Valid promo"
-    else
-        IO.puts "Cant Use Promo"
-    end
+        if radius_origin_to_event <= promo_radius || radius_event_to_destination <= promo_radius do
+            SafeBoda.update_promo(promo, %{is_deactivated: true})
+            {:ok, "Promo code successfully applied"}
+        else
+            {:error, "Distance not within promocode range"}
+        end
+      _             ->
+        {:error, "Invalid promo supplied"}
+      end
+  end
+
+
+  def check_promo_validity(promo) do
+    result = cond do
+              promo.is_deactivated -> {:error, "deactivated"}
+              promo.is_expired     -> {:error, "expired"}
+              true                 -> {:ok, "valid"}
+             end
   end
 end
