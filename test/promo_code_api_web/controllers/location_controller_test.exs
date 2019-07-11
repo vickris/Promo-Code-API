bddce67babf11ca7df3ffd8a8efbe58f911be49b
vickris <christophervundi@gmail.com>
2019-07-11 18:42:01 +0300

    Location controller tests fixed

--- test/promo_code_api_web/controllers/location_controller_test.exs
+++ test/promo_code_api_web/controllers/location_controller_test.exs
@@ -19,26 +19,28 @@ defmodule PromoCodeApiWeb.LocationControllerTest do
 
   describe "index" do
     test "lists all locations", %{conn: conn} do
-      conn = get conn, location_path(conn, :index)
+      conn = get(conn, location_path(conn, :index))
       assert json_response(conn, 200)["data"] == []
     end
   end
 
   describe "create location" do
     test "renders location when data is valid", %{conn: conn} do
-      conn = post conn, location_path(conn, :create), location: @create_attrs
+      conn = post(conn, location_path(conn, :create), location: @create_attrs)
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
-      conn = get conn, location_path(conn, :show, id)
+      conn = get(conn, location_path(conn, :show, id))
+
       assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "latitude" => "120.5",
-        "longitude" => "120.5",
-        "name" => "some name"}
+               "id" => id,
+               "latitude" => 120.5,
+               "longitude" => 120.5,
+               "name" => "some name"
+             }
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, location_path(conn, :create), location: @invalid_attrs
+      conn = post(conn, location_path(conn, :create), location: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -46,20 +48,25 @@ defmodule PromoCodeApiWeb.LocationControllerTest do
   describe "update location" do
     setup [:create_location]
 
-    test "renders location when data is valid", %{conn: conn, location: %Location{id: id} = location} do
-      conn = put conn, location_path(conn, :update, location), location: @update_attrs
+    test "renders location when data is valid", %{
+      conn: conn,
+      location: %Location{id: id} = location
+    } do
+      conn = put(conn, location_path(conn, :update, location), location: @update_attrs)
       assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
-      conn = get conn, location_path(conn, :show, id)
+      conn = get(conn, location_path(conn, :show, id))
+
       assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "latitude" => "456.7",
-        "longitude" => "456.7",
-        "name" => "some updated name"}
+               "id" => id,
+               "latitude" => 456.7,
+               "longitude" => 456.7,
+               "name" => "some updated name"
+             }
     end
 
     test "renders errors when data is invalid", %{conn: conn, location: location} do
-      conn = put conn, location_path(conn, :update, location), location: @invalid_attrs
+      conn = put(conn, location_path(conn, :update, location), location: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -68,11 +75,12 @@ defmodule PromoCodeApiWeb.LocationControllerTest do
     setup [:create_location]
 
     test "deletes chosen location", %{conn: conn, location: location} do
-      conn = delete conn, location_path(conn, :delete, location)
+      conn = delete(conn, location_path(conn, :delete, location))
       assert response(conn, 204)
-      assert_error_sent 404, fn ->
-        get conn, location_path(conn, :show, location)
-      end
+
+      assert_error_sent(404, fn ->
+        get(conn, location_path(conn, :show, location))
+      end)
     end
   end
 
