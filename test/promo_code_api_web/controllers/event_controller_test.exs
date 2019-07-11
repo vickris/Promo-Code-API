3bdfb5b6d36b52b74c5bdae18221c05f8a628169
vickris <christophervundi@gmail.com>
2019-07-11 19:05:25 +0300

    Location controller test in check

--- test/promo_code_api_web/controllers/event_controller_test.exs
+++ test/promo_code_api_web/controllers/event_controller_test.exs
@@ -4,13 +4,19 @@ defmodule PromoCodeApiWeb.EventControllerTest do
   alias PromoCodeApi.SafeBoda
   alias PromoCodeApi.SafeBoda.Event
 
+  @create_location_attrs %{latitude: "120.5", longitude: "120.5", name: "some name"}
   @create_attrs %{location_id: 42, name: "some name"}
   @update_attrs %{location_id: 43, name: "some updated name"}
   @invalid_attrs %{location_id: nil, name: nil}
 
-  def fixture(:event) do
-    {:ok, event} = SafeBoda.create_event(@create_attrs)
+  def fixture(:event, location) do
+    {:ok, event} = SafeBoda.create_event(Map.merge(@create_attrs, %{location_id: location.id}))
     event
+  end
+
+  def fixture(:location) do
+    {:ok, location} = SafeBoda.create_location(@create_location_attrs)
+    location
   end
 
   setup %{conn: conn} do
@@ -19,25 +25,33 @@ defmodule PromoCodeApiWeb.EventControllerTest do
 
   describe "index" do
     test "lists all events", %{conn: conn} do
-      conn = get conn, event_path(conn, :index)
+      conn = get(conn, event_path(conn, :index))
       assert json_response(conn, 200)["data"] == []
     end
   end
 
   describe "create event" do
     test "renders event when data is valid", %{conn: conn} do
-      conn = post conn, event_path(conn, :create), event: @create_attrs
+      location = fixture(:location)
+
+      conn =
+        post(conn, event_path(conn, :create),
+          event: Map.merge(@create_attrs, %{location_id: location.id})
+        )
+
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
-      conn = get conn, event_path(conn, :show, id)
+      conn = get(conn, event_path(conn, :show, id))
+
       assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "location_id" => 42,
-        "name" => "some name"}
+               "id" => id,
+               "location_id" => SafeBoda.get_event!(id).location_id,
+               "name" => "some name"
+             }
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, event_path(conn, :create), event: @invalid_attrs
+      conn = post(conn, event_path(conn, :create), event: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -46,18 +60,24 @@ defmodule PromoCodeApiWeb.EventControllerTest do
     setup [:create_event]
 
     test "renders event when data is valid", %{conn: conn, event: %Event{id: id} = event} do
-      conn = put conn, event_path(conn, :update, event), event: @update_attrs
+      conn =
+        put(conn, event_path(conn, :update, event),
+          event: Map.merge(@update_attrs, %{location_id: event.location_id})
+        )
+
       assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
-      conn = get conn, event_path(conn, :show, id)
+      conn = get(conn, event_path(conn, :show, id))
+
       assert json_response(conn, 200)["data"] == %{
-        "id" => id,
-        "location_id" => 43,
-        "name" => "some updated name"}
+               "id" => id,
+               "location_id" => SafeBoda.get_event!(id).location_id,
+               "name" => "some updated name"
+             }
     end
 
     test "renders errors when data is invalid", %{conn: conn, event: event} do
-      conn = put conn, event_path(conn, :update, event), event: @invalid_attrs
+      conn = put(conn, event_path(conn, :update, event), event: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -66,16 +86,18 @@ defmodule PromoCodeApiWeb.EventControllerTest do
     setup [:create_event]
 
     test "deletes chosen event", %{conn: conn, event: event} do
-      conn = delete conn, event_path(conn, :delete, event)
+      conn = delete(conn, event_path(conn, :delete, event))
       assert response(conn, 204)
-      assert_error_sent 404, fn ->
-        get conn, event_path(conn, :show, event)
-      end
+
+      assert_error_sent(404, fn ->
+        get(conn, event_path(conn, :show, event))
+      end)
     end
   end
 
   defp create_event(_) do
-    event = fixture(:event)
+    location = fixture(:location)
+    event = fixture(:event, location)
     {:ok, event: event}
   end
 end
