cad4ae348a83a854d499e96c0dfbe0333fba6e83
vickris <christophervundi@gmail.com>
2019-07-12 18:21:08 +0300

    Tests passing

--- test/promo_code_api_web/controllers/promo_code_controller_test.exs
+++ test/promo_code_api_web/controllers/promo_code_controller_test.exs
@@ -11,55 +11,63 @@ defmodule PromoCodeApiWeb.PromoCodeControllerTest do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  test "promo usage", %{conn: conn} do
-    origin =
-      Repo.insert!(%Location{
-        name: "Kasarani",
-        latitude: -1.10972,
-        longitude: 37.07692
-      })
+  describe "promo usage" do
+    test "renders promo info when everything is valid", %{conn: conn} do
+      origin =
+        Repo.insert!(%Location{
+          name: "Kasarani",
+          latitude: -1.10972,
+          longitude: 37.07692
+        })
 
-    destination =
-      Repo.insert!(%Location{
-        name: "CBD",
-        latitude: -1.2833,
-        longitude: 36.8167
-      })
+      destination =
+        Repo.insert!(%Location{
+          name: "CBD",
+          latitude: -1.2833,
+          longitude: 36.8167
+        })
 
-    event =
-      Repo.insert!(%Event{
-        name: "Event 1",
-        location_id: origin.id
-      })
+      event =
+        Repo.insert!(%Event{
+          name: "Event 1",
+          location_id: origin.id
+        })
 
-    promo =
-      Repo.insert!(%Promo{
-        code: "CHRISTOPHERVU12",
-        amount: 500,
-        event_id: event.id,
-        radius: 50.0,
-        expiry_date: Date.add(Date.utc_today(), 10)
-      })
+      promo =
+        Repo.insert!(%Promo{
+          code: "CHRISTOPHERVU12",
+          amount: 500,
+          event_id: event.id,
+          radius: 50.0,
+          expiry_date: Date.add(Date.utc_today(), 10)
+        })
 
-    conn =
-      conn
-      |> post(promo_code_path(conn, :request, origin, destination, promo),
-        request_params: %{origin: origin, destination: destination, promo: promo}
-      )
+      conn =
+        conn
+        |> post(promo_code_path(conn, :request, origin, destination, promo),
+          request_params: %{origin: origin, destination: destination, promo: promo}
+        )
 
-    polyline =
-      Polyline.encode([
-        {origin.longitude, origin.latitude},
-        {destination.longitude, destination.latitude}
-      ])
+      polyline =
+        Polyline.encode([
+          {origin.longitude, origin.latitude},
+          {destination.longitude, destination.latitude}
+        ])
 
-    assert json_response(conn, 200)["data"] == %{
-             "amount" => 500,
-             "code" => "CHRISTOPHERVU12",
-             "expiry_date" => "2019-07-22",
-             "is_deactivated" => false,
-             "promo_radius" => 50.0,
-             "polyline" => polyline
-           }
+      assert json_response(conn, 200)["data"] == %{
+               "amount" => 500,
+               "code" => "CHRISTOPHERVU12",
+               "expiry_date" => "2019-07-22",
+               "is_deactivated" => false,
+               "promo_radius" => 50.0,
+               "polyline" => polyline
+             }
+    end
+
+    # test "renders error for invalid data", %{conn: conn} do
+    #   assert_error_sent(:not_found, fn ->
+    #     post(promo_code_path(conn, :request, 1, 1, 22)
+    #   end)
+    # end
   end
 end
